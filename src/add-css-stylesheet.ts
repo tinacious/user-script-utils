a7f143b69678e921be47c6e55e83ed6e6bfb49a6
@tinacious <info@tinaciousdesign.com>
2024-09-29 17:33:46 -0400

    add support for functions evaluation in tagged template literal css

--- src/add-css-stylesheet.ts
+++ src/add-css-stylesheet.ts
@@ -11,9 +11,27 @@ export function addCssStyleSheet(css: string): HTMLStyleElement {
 }
 
 /**
- * Calls {@link addCssStyleSheet} to embed a style element with the provided stylesheet but does not return the element
+ * Allows you to pass functions to evaluate values for the CSS.
+ * Calls {@link addCssStyleSheet} to embed a style element with the provided stylesheet
  * @param cssCode
  */
-export function css(cssCode: string): void {
-  addCssStyleSheet(cssCode)
+export function css(
+  strings: string[],
+  ...values: unknown[]
+): HTMLStyleElement {
+  const evaluated = strings.reduce<string[]>((acc, string, i) => {
+    acc.push(string)
+
+    if (values[i]) {
+      const value = typeof values[i] === 'function' ? values[i]() : values[i].toString()
+      acc.push(value)
+    }
+
+    return acc
+  }, [])
+
+  const rules = evaluated.join('').replace(/ /g, '').split(/\n/)
+  const evaluatedCssString = rules.join('\n')
+
+  return addCssStyleSheet(evaluatedCssString)
 }
